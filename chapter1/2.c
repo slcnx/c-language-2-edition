359081fe7206a04153073d766b25e1d28cd32b8b
slcnx <1062670898@qq.com>
2021-11-19 14:20:10 +0800

    float

--- chapter1/2.c
+++ chapter1/2.c
@@ -6,16 +6,18 @@ print Fahrenheit-Celsius table
 */
 int main(int argc, char const *argv[])
 {
-	int celsius, fahr;
+	float celsius, fahr;
 	int lower, upper, step;
 	lower = 0;
 	upper = 300;
 	step = 20;
 
 	fahr = lower;
+
+	printf("%s\t%s\n", "fahr","celsius");
 	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr,celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32) ;
+		printf("%3.0f\t%6.1f\n", fahr,celsius);
 		fahr = fahr + step;
 	}
 	return 0;
